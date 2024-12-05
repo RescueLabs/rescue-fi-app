8283f0ff6cb16bcce4dd4fabfa544491b01f8c9a
nonseodion <nonseifebhor@gmail.com>
2024-12-05 14:55:58 +0100

    added supense tag for useSearchParams

--- src/app/(default)/faqs/page.tsx
+++ src/app/(default)/faqs/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { AllFaqs } from '@/components/layout/sections/all-faqs';
 import { ScrollArea } from '@/components/ui/scroll-area';
@@ -12,7 +12,9 @@ const FAQPage = () => {
   return (
     <ScrollArea className="flex h-[calc(100dvh-60px)] w-full flex-col gap-y-7 overflow-y-auto overflow-x-hidden md:h-full">
       <section className="mx-auto flex w-full max-w-[800px] flex-col items-center gap-y-20 px-3 py-10">
-        <AllFaqs />
+        <Suspense>
+          <AllFaqs />
+        </Suspense>
       </section>
     </ScrollArea>
   );
