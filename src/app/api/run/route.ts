9953bf1efb8d506cdcf94287fe29a4c0a9a3cd71
caspero-62 <nwaforuchechukwu2007@gmail.com>
2025-08-06 13:26:34 +0100

    fix: build error

--- src/app/api/run/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-import signStuff from '../../../../signstuff';
-
-export async function GET(request: NextRequest) {
-  const result = await signStuff();
-  return NextResponse.json(result);
-}
